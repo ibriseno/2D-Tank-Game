3e3cfd3e4121709001066fd194a79d00a30b1e53
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-11-07 18:27:44 -0800

    The public void update class now shoots the bullet in a straight path depending on the angle and coordinates of the tank. drawImage draws the bullet into GameWorld.

--- TankGame/Bullet.java
+++ TankGame/Bullet.java
@@ -6,15 +6,18 @@ import java.awt.image.BufferedImage;
 
 public class Bullet  {
 
-    int Xspeed, damage, angle;
-    int x, y, vx, vy;
-    int Yspeed;
-    boolean show;
+    private int Xspeed;
+    private int damage;
+    private int angle;
+    private int x;
+    private int  y;
+    private int vx;
+    private int vy;
+    private final int R = 2;
+    private int Yspeed;
     private BufferedImage img;
 
-    private boolean enterPressed;
-
-    Bullet(BufferedImage img, int x, int y, int vy, int vx, int angle, int damage, int Xspeed, int Yspeed){
+    Bullet(BufferedImage img, int x, int y, int angle, int damage, int Xspeed, int Yspeed){
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -22,32 +25,31 @@ public class Bullet  {
         this.img = img;
         this.Xspeed = Xspeed;
         this.Yspeed = Yspeed;
-        this.damage = damage;
         this.angle = angle;
-        this.show = true;
+
     }
-    public boolean getShow(){
-        return this.show;
-    }
-    public void setShow(boolean s){
-        this.show = s;
-    }
+
+    /**
+     * The update keeps the bullet on a straight path
+     * depending on the angle of the tank.
+     * @param w
+     * @param h
+     */
     public void update(int w, int h){
-        if(y < h-40 && y > 0 && x > 0 && x < w-40 && show){
-            x = x + Xspeed;
-            y = y + Yspeed;
-        }else{
-            this.show = false;
-        }
-    }
-    public void drawBullet(Graphics g){
-        AffineTransform rotation = AffineTransform.getTranslateInstance(x, y);
-        AffineTransform shoot = AffineTransform.getTranslateInstance(x, y);
-
-        rotation.rotate(Math.toRadians(angle), this.img.getWidth() / 2.0, this.img.getHeight() / 2.0);
-        //Graphics2D g2d = (Graphics2D) g;
-        Graphics2D b2d = (Graphics2D) g;
-        b2d.drawImage(this.img, shoot, null);
+        vx = (int) Math.round((R * Math.cos(Math.toRadians(angle))));
+        vy = (int) Math.round((R * Math.sin(Math.toRadians(angle))));
+        x += vx;
+        y += vy;
     }
 
+
+    public void drawImage(Graphics g) {
+            AffineTransform rotation = AffineTransform.getTranslateInstance(x, y);
+            AffineTransform shoot = AffineTransform.getTranslateInstance(x, y);
+
+            shoot.rotate(Math.toRadians(angle), this.img.getWidth()/2.0, this.img.getHeight()/2.0);
+
+            Graphics2D b2d = (Graphics2D) g;
+            b2d.drawImage(this.img, shoot, null);
+    }
 }
